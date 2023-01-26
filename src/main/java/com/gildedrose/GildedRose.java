b296e0ebe0679f922c4a4565b0b234a3c6ee94be
arij95 <arij.mediouni@esprit.tn>
2023-01-26 10:02:00 +0100

    Invert if conditions
    Invert if conditions and add comments to better understand the code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,24 +9,19 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
+            // If item name equals to Aged Brie or Backstage passes and quantity < 50 quantity will increase
+            if (items[i].name.equals("Aged Brie")
+                || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
-
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        // If item name equals to Backstage passes and quantity < 50 and sellIn < 11 quantity will increase again
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
-
+                        // If item name equals to Backstage passes and quantity < 50 and sellIn < 6 quantity will increase again
                         if (items[i].sellIn < 6) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -34,26 +29,41 @@ class GildedRose {
                         }
                     }
                 }
+            } else {
+                if (items[i].quality > 0) {
+                    // if item name equals to sulfuras nothing happens
+                    if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    } else {
+                        // if quantity is positive it decreases by 1
+                        items[i].quality = items[i].quality - 1;
+                    }
+                }
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            // if item name equals to sulfuras nothing happens
+            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            } else {
+                // sellIn decreases by 1
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
+                // if sellIn is negative, quantity < 50 and item name equals to Aged Brie quantity increase again
+                if (items[i].name.equals("Aged Brie")) {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
+                    }
+                } else {
+                    // if sellIn is negative and item name equals to Backstage passes quantity increase again
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    } else {
+                        if (items[i].quality > 0) {
+                            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                                continue;
+                            }
+                            // if sellIn is negative, quality is positive and item name is not equal to Sulfuras quantity decreases again
+                            items[i].quality = items[i].quality - 1;
+                        }
                     }
                 }
             }
